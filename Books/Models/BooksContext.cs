251e50a3dba55ed8fdc68d137bc1c9acfdb33510
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-07-05 16:15:16 +0300

    Add project files.

---
+++ Books/Models/BooksContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Books.Models
+{
+    public partial class BooksContext : DbContext
+    {
+
+
+        public BooksContext(DbContextOptions<BooksContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Book { get; set; }
+
+
+    }
+}
